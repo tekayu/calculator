08a634bee16ce9c066cc92f6d9948a62869ceeb8
tekayu <joshuaodb@icloud.com>
2025-05-07 14:30:17 +1000

    added calculator functionality

--- script.js
+++ script.js
@@ -4,18 +4,68 @@ const operators = document.querySelectorAll(".btn-operator")
 const clear = document.querySelector(".btn-clear")
 const equals = document.querySelector(".btn-equals")
 
-let displayValue = ""
+let displayValue = "";
+let currOp = "";
+let opValue = "";
 
 numbers.forEach((num) => {
     num.addEventListener("click", () => {
-        displayValue += num.textContent;
+        if (currOp === "") {
+            displayValue += num.textContent;
+            display.textContent = displayValue;
+            return
+        } else {
+            opValue += num.textContent
+            display.textContent = opValue
+            return
+        }
+    }); 
+})
+
+equals.addEventListener("click", () => {
+    if (currOp === "") {
         display.textContent = displayValue;
-    });
+    }
+    if (currOp == "+") {
+        displayValue = parseInt(displayValue) + parseInt(opValue);
+        display.textContent = displayValue;
+        currOp == "";
+        opValue == "";
+    }
+    if (currOp == "-") {
+        displayValue = parseInt(displayValue) - parseInt(opValue)
+        display.textContent = displayValue
+        currOp == "";
+        opValue == "";
+    }
+    if (currOp == "*") {
+        displayValue = parseInt(displayValue) * parseInt(opValue)
+        display.textContent = displayValue
+        currOp == "";
+        opValue == "";
+    }
+    if (currOp == "/") {
+        displayValue = parseInt(displayValue) / parseInt(opValue)
+        display.textContent = displayValue
+        currOp == "";
+        opValue == "";
+    }
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if (currOp === "") {
+            currOp = operator.getAttribute("id");
+        } else {
+            opValue = "";
+            currOp = operator.getAttribute("id");
+        }
+    })
 })
 
 clear.addEventListener("click", () => {
     displayValue = "";
-    display.textContent = displayValue;
+    opValue = "";
+    currOp = "";
+    display.textContent = "";
 })
-
-equals.addEventListener("click", () => console.log(displayValue));
